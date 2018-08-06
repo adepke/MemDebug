4fa3a1f6487e1a35a843eeb73a33b81f1a2d31ed
XenonicDev <andrewdepke@gmail.com>
2018-08-05 20:15:01 -0600

    Added Documentation, Copyright Notice

--- MemDebug/Include/MemDebug.h
+++ MemDebug/Include/MemDebug.h
@@ -1,3 +1,6 @@
+/*
+	Copyright (c) 2018 Andrew Depke
+*/
 #pragma once
 
 #include <map>
@@ -7,31 +10,49 @@
 
 namespace MemDebug
 {
+	/*
+	* Total Memory Allocated in Bytes This Session.
+	*/
 	size_t GetTotalAllocated()
 	{
 		return Allocator::Get().TotalAllocated;
 	}
 
+	/*
+	* Total Memory Released in Bytes This Session.
+	*/
 	size_t GetTotalDeallocated()
 	{
 		return Allocator::Get().TotalDeallocated;
 	}
 
+	/*
+	* Total Memory Allocated and Not Yet Released in Bytes This Session.
+	*/
 	size_t GetCurrentlyAllocated()
 	{
 		return Allocator::Get().CurrentlyAllocated;
 	}
 
+	/*
+	* Most Recent Allocation in Bytes.
+	*/
 	size_t GetLastAllocated()
 	{
 		return Allocator::Get().LastAllocated;
 	}
 
+	/*
+	* Most Recent Release in Bytes.
+	*/
 	size_t GetLastDeallocated()
 	{
 		return Allocator::Get().LastDeallocated;
 	}
 
+	/*
+	* Retrieve a List of All Currently Allocated Memory Blocks, Includes Debugging Information for Each Block.
+	*/
 	std::map<void*, Block>* GetBlocks()
 	{
 		return &Allocator::Get().BlockMap;
@@ -51,39 +72,60 @@ namespace MemDebug
 
 namespace MemDebug
 {
+	/*
+	* Total Memory Allocated in Bytes This Session.
+	*/
 	size_t GetTotalAllocated()
 	{
 		return 0;
 	}
 
+	/*
+	* Total Memory Released in Bytes This Session.
+	*/
 	size_t GetTotalDeallocated()
 	{
 		return 0;
 	}
 
+	/*
+	* Total Memory Allocated and Not Yet Released in Bytes This Session.
+	*/
 	size_t GetCurrentlyAllocated()
 	{
 		return 0;
 	}
 
+	/*
+	* Most Recent Allocation in Bytes.
+	*/
 	size_t GetLastAllocated()
 	{
 		return 0;
 	}
 
+	/*
+	* Most Recent Release in Bytes.
+	*/
 	size_t GetLastDeallocated()
 	{
 		return 0;
 	}
 
+	/*
+	* Retrieve a List of All Currently Allocated Memory Blocks, Includes Debugging Information for Each Block.
+	*/
 	std::map<void*, Block>* GetBlocks()
 	{
 		return nullptr;
 	}
 }  // namespace MemDebug
 
-#define PUSH_HEAP_NAME(Ignored) void(0)
-#define POP_HEAP_NAME() void(0)
-#define CLEAR_HEAP_NAMES() void(0)
+// Push a Heap Name Onto the Name Stack.
+#define PUSH_HEAP_NAME(Ignored) (void)0
+// Pop a Heap Name Off the Name Stack.
+#define POP_HEAP_NAME() (void)0
+// Empty the Heap Name Stack.
+#define CLEAR_HEAP_NAMES() (void)0
 
 #endif
--- MemDebug/Source/Block.h
+++ MemDebug/Source/Block.h
@@ -1,3 +1,6 @@
+/*
+	Copyright (c) 2018 Andrew Depke
+*/
 #pragma once
 
 #define MAX_HEAP_NAME_LENGTH 32
@@ -12,9 +15,13 @@ namespace MemDebug
 {
 	struct Block
 	{
+		// Size, in Bytes of the Block.
 		size_t Size;
+		// Name of the Region in Which the Block was Allocated.
 		char Name[MAX_HEAP_NAME_LENGTH];
+		// Name of the File Where the Block was Allocated.
 		char Filename[MAX_FILENAME_LENGTH];
+		// Approximated Line Number of the File Where the Block was Allocated.
 		int Line;
 	};
 }  // namespace MemDebug
